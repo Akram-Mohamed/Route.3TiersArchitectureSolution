699d9ec4be71d2e7dffa213ca10e9f65df15b2cc
akram <akram@66F1E127D275C36>
2024-03-20 05:04:44 +0200

    Add project files.

---
+++ Route.3TiersArchitecture.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Route._3TiersArchitecture.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
