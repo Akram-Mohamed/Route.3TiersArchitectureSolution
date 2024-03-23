97d908875bbfa605a42e3c24297e2f2f7d48054e
Akram Mohamed <akrammohamed508@gmail.com>
2024-03-23 22:05:56 +0200

    Employee Controller Creation

---
+++ Route.3TiersArchitecture.DAL/Data/Configrations/EmployeeConfigrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route._3TiersArchitecture.DAL.Models_Services_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route._3TiersArchitecture.DAL.Data.Configrations
+{
+    public class EmployeeConfigrations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> employee_builder)
+        {
+            employee_builder.HasKey(E => E.Emp_Id);
+        }
+    }
+}
