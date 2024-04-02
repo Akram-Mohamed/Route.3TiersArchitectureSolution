1e6f76c87e9c44b51d3ceb7afbae361c5b190cf2
Akram Mohamed <akrammohamed508@gmail.com>
2024-04-02 11:53:28 +0200

    Refactor Create Employee Action - Upload Image

---
+++ Route.3TiersArchitecture.DAL/Data/Migrations/20240402094550_ImageNameColumForEmployee.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Route._3TiersArchitecture.DAL.Data.Migrations
+{
+    public partial class ImageNameColumForEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Employees",
+                newName: "ISActive");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "ISActive",
+                table: "Employees",
+                newName: "IsActive");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Employees",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
---
+++ Route.3TiersArchitecture.PL/Models/EmployeeResponseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Route._3TiersArchitecture.DAL.Models_Services_;
+using System;
+
+namespace Route._3TiersArchitecture.PL.Models
+{
+    public class EmployeeResponseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+        public bool ISActive { get; set; }
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string ImageName { get; set; }
+   
+   
+
+
+    }
+}
